bfafa823b5c1a6a7489715e4ff84a9f867526a76
Thouhedul Islam <thouhedul@cartx.io>
2020-09-10 22:30:46 +0600

    Added left menu

---
+++ en/leftnav.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'automated_messages' => [
+        'title' => 'Automated Messages',
+        'automated_cart' => 'Automated Cart',
+    ],
+    'manual_messages' => [
+        'title' => 'Manual Messages',
+        'abandoned_cart' => 'Abandoned Cart',
+        'boleto_reminder' => 'Boleto Reminder',
+        'cancelled_order' => 'Cancelled Order',
+        'completed_order' => 'Completed Order',
+    ],
+    'analytics' => [
+        'title' => 'Analytics (Comming soon)',
+        'automated' => 'Automated',
+        'manual_cart' => 'Manual Cart',
+    ],
+    'settings' => [
+        'title' => 'Settings',
+        'settings' => 'Settings',
+    ],
+];
